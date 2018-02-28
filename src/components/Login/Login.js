c65e2d0a4fb6caeba655821aaa2069fac23bda63
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-02-28 13:50:38 -0700

    added login page skeleton

---
+++ src/components/Login/Login.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import './Login.css';
+
+class Login extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    }
+
+  }
+
+  componentDidMount(){
+    
+  }
+
+  render() {
+    return (
+      <section className='login_page'>
+        
+      </section>
+    );
+  }
+}
+
+
+export default Login;
--- src/router.js
+++ src/router.js
@@ -2,13 +2,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Login from './components/Login/Login.js';
 import Home from './components/Home/Home.js';
 
 
 export default (
     <Switch>
         
-        <Route component={ Home } path='/' exact />
+        <Route component={ Login } path='/' exact />
+        <Route component={ Home } path='/home' exact />
 
     </Switch>
 )
