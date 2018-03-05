07a419f56a3fac6617f2c69687be853569423052
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-03-05 13:34:49 -0700

    adding logic to the login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 import './Login.css';
 
@@ -9,16 +10,32 @@ class Login extends Component {
     super(props);
 
     this.state = {
-
+      username: '',
+      password: '',
+      userPic: '',
+      loginMethodFR: true,
     }
 
+    this.toggleLoginMethod = this.toggleLoginMethod.bind(this);
+    this.login = this.login.bind(this);
   }
 
-  componentDidMount(){
-    
+  toggleLoginMethod(){
+    this.setState({
+      loginMethodFR: !this.state.loginMethodFR
+    })
+  }
+
+  login(){
+    if (this.state.loginMethodFR){
+      //take pic, axios username and pic login (use facial recognition api)
+    }else{
+      // axios username/password login
+    }
   }
 
   render() {
+    let newMethod = this.state.loginMethodFR ? 'Password' : 'Facial Recognition';
     return (
       <section className='login'>
 
@@ -27,11 +44,19 @@ class Login extends Component {
         </div>
 
         <div className='login_form'>
-          <p className='mcenter tcenter'>Login Here</p>
-          <div className='login_input_div'><input placeholder='username' /></div>
-          <div className='login_input_div'><input placeholder='password' type='password' /></div>
-          <button>Login</button>
-          <button>Facial Recognition Login</button>
+          <p className='mcenter tcenter'>Login to Your Account</p>
+          <div className='login_input_div'><input placeholder='username' value={this.state.username} onChange={(e) => this.setState({username: e.target.value})} /></div>
+          {
+            this.state.loginMethodFR ?  
+              <div className='fr_login'>
+                <button onClick={this.login} >Login</button>
+              </div>
+            : <div className='login_input_div'>
+                <input placeholder='password' value={this.state.password} onChange={(e) => this.setState({password: e.target.value})} type='password' />
+                <button onClick={this.login} >Login</button>
+              </div>
+          }
+          <p className='toggle_method' onClick={this.toggleLoginMethod} >Login With {newMethod} Instead</p>
         </div>
 
       </section>
